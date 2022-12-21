991323e427144b37e65c38d8b5efbda86ac9271f
derikbf <derikb@gmail.com>
2022-12-21 14:08:07 -0300

    Add Opposite Number - 8KYU challenge

---
+++ Ruby/8KYU/OppositeNumber.rb
@@ -0,0 +1,19 @@
+# Opposite number
+
+# DESCRIPTION:
+
+# Very simple, given an integer or a floating-point number, find its opposite.
+
+# Examples:
+# 1: -1
+# 14: -14
+# -34: 34
+
+# Solution
+
+def opposite(number)
+  -number
+end
+
+# run
+puts opposite(10)
