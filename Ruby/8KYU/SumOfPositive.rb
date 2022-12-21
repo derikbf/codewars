009ceab6c4b01d7b1e8068b01b24334c823f50ab
derikbf <derikb@gmail.com>
2022-12-21 14:11:19 -0300

    Add Sum of Positive - 8KYU challenge

---
+++ Ruby/8KYU/SumOfPositive.rb
@@ -0,0 +1,18 @@
+# Sum Of Positive
+
+# DESCRIPTION:
+
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: if there is nothing to sum, the sum is default to 0.
+
+# Solution
+
+def positive_sum(arr)
+  arr.select { |num| num > 0 }.sum
+end
+
+# run
+puts positive_sum(arr = [1,-4,7,12])
