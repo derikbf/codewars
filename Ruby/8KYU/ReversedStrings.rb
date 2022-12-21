a5d966d2bbecce220c6f89bb6ef9b565f7dc75dc
derikbf <derikb@gmail.com>
2022-12-21 14:09:49 -0300

    Add Reversed Strings - 8KYU challenge

---
+++ Ruby/8KYU/ReversedStrings.rb
@@ -0,0 +1,17 @@
+# Reversed Strings
+
+# DESCRIPTION:
+
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# Solution
+
+def solution(str)
+  str.reverse
+end
+
+# run
+puts solution("ABCDE")
