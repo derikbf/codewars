9ea83814f2d8ebb59a8f926524fd9d99c1640e18
derikbf <derikb@gmail.com>
2022-12-21 14:05:40 -0300

    Add Convert Boolean Values to String - 8KYU challenge

---
+++ Ruby/8KYU/ConvertBooleanValuesToStringsYesOrNo.rb
@@ -0,0 +1,15 @@
+# Convert boolean values to strings 'Yes' or 'No'
+
+# DESCRIPTION:
+
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+# Solution
+
+def bool_to_word bool
+  bool == true ? "Yes" : "No"
+end
+
+# run
+# puts bool_to_word(false)
+puts bool_to_word(true)
