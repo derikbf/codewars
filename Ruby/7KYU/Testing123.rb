0e97e1316a769ac371c56606a6b41e4ccd07efb1
derikbf <derikb@gmail.com>
2022-12-26 20:37:18 -0300

    Add Testing 1-2-3 - 7KYU challenge

---
+++ Ruby/7KYU/Testing123.rb
@@ -0,0 +1,22 @@
+# Testing 1-2-3
+
+# DESCRIPTION:
+# Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+# Write a function which takes a list of strings and returns each line prepended by the correct number.
+# The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+# Examples: (Input --> Output)
+# [] --> []
+# ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+# Solution                                                                                                                                                                                                                                   # Solution
+
+def number(lines)
+  lines.map.with_index(1)  do | line, index |
+    "#{index}: #{line}"
+  end
+end
+
+# run
+print number([])
+print number(["a", "b", "c"])
