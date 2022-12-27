40b19e85d68b896de2e03eda057f0b78621576df
derikbf <derikb@gmail.com>
2022-12-26 21:24:02 -0300

    Add MakeUpperCase - 8KYU challenge

---
+++ Ruby/8KYU/MakeUpperCase.rb
@@ -0,0 +1,13 @@
+# MakeUpperCase
+
+# DESCRIPTION:
+# Write a function which converts the input string to uppercase.
+
+# Solution                                                                                                                                                                                                                                   # Solution
+
+def make_upper_case(str)
+  str.upcase
+end
+
+# run
+puts make_upper_case("hello")
